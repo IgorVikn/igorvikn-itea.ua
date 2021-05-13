3f79ed7cf11db114ad88fb5f5ea05a9336cae6f5
IgorVikn <igorvikn@gmail.com>
2021-05-13 18:09:17 +0300

    save installed composer and package guzzlehttp

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 
-	echo "My page";
-<<<<<<< HEAD
-	echo "Continue changes on branch Master";
-=======
-	echo "My file on new-branch";
->>>>>>> new-branch
-	echo phpinfo();
+	echo "My page" . PHP_EOL;
+	echo "Continue changes on branch Master" . PHP_EOL . "<br>";
+
+	use igorvikn\math\Multipl ;
+
+	include 'vendor/autoload.php';
+
+	$Mymath = new Multipl();
+	echo $Mymath->multiply(5, 5);
+
+//echo phpinfo();
 
 
 ?>
